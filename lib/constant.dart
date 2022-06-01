80284a2973912ab0f7bda011e8ef551e97a4660b
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2022-06-01 13:19:15 +0700

    add constants

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFFFFFFFF);
+const kBackgroundDarkColor = Color(0xFFF8F8F8);
+const kTextColor = Color(0xFF78B3AA);
+const kButtonColor = Color(0xFF64B5AC);
+
+const kPadding = 20.0;
