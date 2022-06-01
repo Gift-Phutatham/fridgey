f1b7c83aa0b3b1ddb2f099a5bfacb5e2fd786ca3
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2022-06-01 14:02:44 +0700

    add mock data

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -51,4 +51,50 @@ class Product {
   String toString() {
     return 'Product(Id: $id, Product name: $productName, Category: $category, Quantity: $quantity, Unit: $unit)';
   }
+
+  /// Mock data
+  List<Product> products = [
+    const Product(
+      id: 1,
+      productName: "productName1",
+      category: "category1",
+      quantity: 1,
+      unit: "unit1",
+    ),
+    const Product(
+      id: 2,
+      productName: "productName2",
+      category: "category2",
+      quantity: 2,
+      unit: "unit2",
+    ),
+    const Product(
+      id: 3,
+      productName: "productName3",
+      category: "category3",
+      quantity: 3,
+      unit: "unit3",
+    ),
+    const Product(
+      id: 4,
+      productName: "productName4",
+      category: "category4",
+      quantity: 4,
+      unit: "unit4",
+    ),
+    const Product(
+      id: 5,
+      productName: "productName5",
+      category: "category5",
+      quantity: 5,
+      unit: "unit5",
+    ),
+    const Product(
+      id: 6,
+      productName: "productName6",
+      category: "category6",
+      quantity: 6,
+      unit: "unit6",
+    ),
+  ];
 }
