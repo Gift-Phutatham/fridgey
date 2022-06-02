bf01e79a8e5860461ab111c74cad8aa64cdcec6f
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2022-06-02 15:18:17 +0700

    refactor code

--- lib/components/product_list.dart
+++ lib/components/product_list.dart
@@ -14,8 +14,7 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
-  late List<Product> products = [];
-  late Future<List<Product>> temp;
+  late Future<List<Product>> products;
 
   @override
   void initState() {
@@ -25,14 +24,14 @@ class _ProductListState extends State<ProductList> {
 
   void refreshProducts() async {
     setState(() {
-      temp = FridgeyDb.instance.readProducts();
+      products = FridgeyDb.instance.readProducts();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: temp,
+      future: products,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (snapshot.hasData) {
           return ListView.builder(
@@ -125,100 +124,3 @@ String getImage(String product) {
   }
   return toReturn;
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:fridgey/pages/update_page.dart';
-//
-// import '../models/product.dart';
-// import '../pages/update_page.dart';
-//
-// class ProductList extends StatefulWidget {
-//   const ProductList({Key? key}) : super(key: key);
-//
-//   @override
-//   State<ProductList> createState() => _ProductListState();
-// }
-//
-// class _ProductListState extends State<ProductList> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       scrollDirection: Axis.vertical,
-//       itemCount: products.length,
-//       itemBuilder: (item, index) => GestureDetector(
-//         child: Container(
-//           margin: const EdgeInsets.only(top: 5.0, bottom: 5.0, right: 3.0),
-//           padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-//           child: Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//             children: <Widget>[
-//               Row(
-//                 children: <Widget>[
-//                   Image(
-//                     image: AssetImage(getImage(products[index].category)),
-//                     width: 50,
-//                     height: 50,
-//                   ),
-//                   const SizedBox(width: 10.0),
-//                   Column(
-//                     children: <Widget>[
-//                       Text(
-//                         products[index].productName,
-//                       ),
-//                       const SizedBox(height: 5.0),
-//                       Text(
-//                         '${products[index].quantity.toString()} ${products[index].unit}',
-//                       )
-//                     ],
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                   ),
-//                 ],
-//               ),
-//               const Image(
-//                 image: AssetImage('assets/next.png'),
-//                 width: 20,
-//                 height: 20,
-//               ),
-//             ],
-//           ),
-//         ),
-//         onTap: () => Navigator.push(
-//           context,
-//           MaterialPageRoute(builder: (_) => const UpdatePage()),
-//         ),
-//       ),
-//     );
-//   }
-// }
-//
-// String getImage(String product) {
-//   String toReturn = '';
-//   switch (product) {
-//     case 'Starch & Grains':
-//       {
-//         toReturn = 'assets/starch_grains.png';
-//       }
-//       break;
-//     case 'Fruits & Vegetables':
-//       {
-//         toReturn = 'assets/fruits_vegetables.png';
-//       }
-//       break;
-//     case 'Meat & Fish':
-//       {
-//         toReturn = 'assets/meat_fish.png';
-//       }
-//       break;
-//     case 'Dairy':
-//       {
-//         toReturn = 'assets/dairy.png';
-//       }
-//       break;
-//     case 'Others':
-//       {
-//         toReturn = 'assets/others.png';
-//       }
-//       break;
-//   }
-//   return toReturn;
-// }
