8d9a4378fb02b53e08edb47c8b79ed780a161928
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2022-06-07 15:38:28 +0700

    change create and update pages from stateful to stateless

--- lib/pages/create_page.dart
+++ lib/pages/create_page.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 
 import '../components/data_entry.dart';
 
-class CreatePage extends StatefulWidget {
+class CreatePage extends StatelessWidget {
   const CreatePage({Key? key}) : super(key: key);
 
-  @override
-  State<CreatePage> createState() => _CreatePageState();
-}
-
-class _CreatePageState extends State<CreatePage> {
   @override
   Widget build(BuildContext context) {
     return const DataEntry(
--- lib/pages/update_page.dart
+++ lib/pages/update_page.dart
@@ -3,22 +3,17 @@ import 'package:flutter/material.dart';
 import '../components/data_entry.dart';
 import '../models/product.dart';
 
-class UpdatePage extends StatefulWidget {
+class UpdatePage extends StatelessWidget {
   final Product product;
 
   const UpdatePage({Key? key, required this.product}) : super(key: key);
 
   @override
-  State<UpdatePage> createState() => _UpdatePageState();
-}
-
-class _UpdatePageState extends State<UpdatePage> {
-  @override
   Widget build(BuildContext context) {
     return DataEntry(
       kind: 1,
       title: 'Edit Product',
-      product: widget.product,
+      product: product,
     );
   }
 }
